73a14c563e09581f8ce8a1e8bd52761841ea1dbc
Danilo <danilocamargodeoliveira97@gmail.com>
2023-05-08 15:26:12 -0300

    Create CRUD.py

---
+++ CRUD.py
@@ -0,0 +1,38 @@
+import pyodbc
+#passamos todos os parametros para conexão com banco de dados, no meu caso está no https://app.planetscale.com/danilocamargodeoliveira97/banco_dadospython
+driver = "MySQL ODBC 8.0 ANSI Driver"
+server = "aws.connect.psdb.cloud"
+database = "banco_dadospython"
+username = "i3uxxaay8qz0ymraoi01"
+password = "pscale_pw_3lFm8NvAY2RFvJOjsq7egpSQfVrX8COlsfqW7APxU2"
+string_conexao = f"DRIVER={driver};SERVER={server};DATABASE={database};UID={username};PWD={password}"
+conexao = pyodbc.connect(string_conexao)
+
+cursor = conexao.cursor()
+
+#Vamos Para o Crud , no caso no banco de dados ja temos a tabela cadastros com campos id,nome,email
+#Create
+'''nome=str(input('Digite um Nome para cadastro :')) #danilo    
+email=str(input('Digite um email para cadastro :')) #danilo1997@hotmail.com
+comando_create = f'INSERT INTO cadastros (nome,email) VALUES ("{nome}","{email}")'
+cursor.execute(comando_create)
+conexao.commit()#edita banco de dados'''
+#Read
+'''comando_ler = f'SELECT * FROM cadastros'
+cursor.execute(comando_ler)
+ler = cursor.fetchall()
+print(ler)
+cursor.close()
+conexao.close()'''
+#Update
+'''comando_update = f'UPDATE cadastros set email = "danilo1997@hotmail.com" where id = "5"'
+cursor.execute(comando_update)
+conexao.commit()#edita banco de dados
+cursor.close()
+conexao.close()'''
+#Delete
+'''comando_update = f'DELETE FROM cadastros where nome="Danilo"'
+cursor.execute(comando_update)
+conexao.commit()#edita banco de dados
+cursor.close()
+conexao.close()'''
